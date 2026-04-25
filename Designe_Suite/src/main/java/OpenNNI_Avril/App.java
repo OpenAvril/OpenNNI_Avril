4a93f5269d967df0b99838c272d7dad207508261
WriteEnableQue, LaunchEnableQue, OpenEpicentre, OpenAvrilCFSD,  OpenAvrilNNI. <65234262+OpenAvril@users.noreply.github.com>
2026-04-25 13:00:57 +1000

    Add files via upload
    initial commit.

---
+++ Designe_Suite/src/main/java/OpenNNI_Avril/App.java
@@ -0,0 +1,13 @@
+package OpenNNI_Avril;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
